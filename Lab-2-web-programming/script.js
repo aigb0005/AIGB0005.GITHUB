1dc70ab98348131df4cd68d3d3716de52c03e6af
aigb0005 <aigb0005@algonquinlive.com>
2023-09-19 09:15:00 -0400

    Completes the Lab 2

---
+++ Lab-2-web-programming/script.js
@@ -0,0 +1,12 @@
+//let declaration
+//do not include value during declaration
+let name = "olivia"
+//const
+//does not allow ressignment
+//does need value during declaration
+const title = "Student"
+// title = student
+console.log ("my name is" + name ) 
+let number = 7
+let sqaured = 49
+console.log ("The squared of" + number )
